f8cb520ae97077db59ded3b7414361227ffbc976
Andrea Geremicca <geremicca.andrea@iisgalvanimi.edu.it>
2024-01-04 10:17:56 +0000

    Punto4

--- Geremicca/src/app/macchina/macchina.component.ts
+++ Geremicca/src/app/macchina/macchina.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-macchina',
   templateUrl: './macchina.component.html',
   styleUrls: ['./macchina.component.css']
 })
-export class MacchinaComponent {
-
-}
+export class MacchinaComponent implements OnInit {
+    @Input() macchina: string; // <-- Aggiungi la notazione @input
+  
+    constructor() {
+       
+    }
+  
+    ngOnInit() {}
+  }
