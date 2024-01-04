3f3ac46512289c8ae5ef0c8b10e25efd2ad1fcaf
Andrea Geremicca <geremicca.andrea@iisgalvanimi.edu.it>
2024-01-04 09:59:14 +0000

    Punto2

--- Geremicca/src/app/container/container.component.ts
+++ Geremicca/src/app/container/container.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-container',
   templateUrl: './container.component.html',
   styleUrls: ['./container.component.css']
 })
-export class ContainerComponent {
+export class ContainerComponent implements OnInit{
+  macchine: string[]; //Creiamo il nostro vettore di stringhe 
+  constructor() { 
+     this.macchine = ['Opel', 'Fiat', 'Toyota', 'Audi', 'BMW', 'Lamborghini', 'Ferrari', 'McClaren', 'Porche', 'Alfa Romeo']; //Riempiamo il vettore
+  }
+  ngOnInit() {
+  }
 
 }
